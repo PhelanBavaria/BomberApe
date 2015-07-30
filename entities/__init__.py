87c95cec92226170d55a7dfb34734194c6f49339
PhelanBavaria <antextides@gmail.com>
2015-07-30 21:15:53 +0200

    added method to die for entities

--- entities/__init__.py
+++ entities/__init__.py
@@ -6,6 +6,7 @@ import yaml
 
 class Entity:
     def __init__(self, id, game, pos, item=None):
+        self.alive = True
         self.id = id
         self.game = game
         self.pos = pos
@@ -13,10 +14,16 @@ class Entity:
         self.stats = yaml.load(id)
         self.item = item
         self.appendages = []
+        self.on_death = {
+            'detonate': self._detonate
+        }
 
     def _move(self, x=0, y=0):
         self.pos[0] += x
         self.pos[1] += y
+
+    def _detonate(self):
+        print('DEBUG: Detonated')
 
     def move_left(self):
         self._move(x=1)
@@ -36,3 +43,10 @@ class Entity:
                            self.item.stats['max_uses'])
             if max_uses > len(self.appendages):
                 self.item.use(self)
+
+    def die(self):
+        for appendage in self.appendages:
+            appendage.die()
+        self.alive = False
+        if self.stats['on_death']:
+            self.on_death[self.stats['on_death']]()
