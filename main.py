05f87ab93123d3d0fb3dbc03aa0f85fee7abbed3
PhelanBavaria <antextides@gmail.com>
2015-07-30 19:00:40 +0200

    can now create a match

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 
 
+import random
+
+
 class Game:
-    pass
+    def create_match(self):
+        width = random.randint(25, 50)
+        height = random.randint(25, 50)
+        tiles = {}
+        for y in range(height):
+            for x in range(width):
+                tiles[(x, y)] = 'grass'
 
 
 if __name__ == '__main__':
+    random.seed(555)
     game = Game()
+    game.create_match()
